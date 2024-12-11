273bda4a743a68dcf1bf140e3e8f311714d5e3e1
Jav2000 <javierrodriguezsan2000@gmail.com>
2024-12-11 20:48:53 +0100

    Hide Django key

--- coupleApp/coupleApp/settings.py
+++ coupleApp/coupleApp/settings.py
@@ -18,11 +18,9 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j1@8!r()il&wh5e14j=oh&(kzd(1ogf=p2!qz41=z^0!00rk8s'
+# .env variables
+SECRET_KEY = config('SECRET_KEY')
+GOOGLE_MAPS_API_KEY = config('GOOGLE_MAPS_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,5 +127,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Directorio donde se almacenan lo
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-GOOGLE_MAPS_API_KEY = config('GOOGLE_MAPS_API_KEY')
